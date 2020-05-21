456f409ab1b1aceeabd8a7e326723951599dcd5c
harshithva <harshith11032001@gmail.com>
2020-05-21 23:12:57 +0530

    Able to scrape

---
+++ index.js
@@ -0,0 +1,43 @@
+const request = require('./node_modules/request-promise');
+const cheerio = require('cheerio');
+// const iconv = require('./node_modules/iconv-lite');
+// var encoding = 'utf-8';
+
+const site = "https://beebom.com/android-tv-background-audio-streaming/";
+
+(async () => {
+    let data = [];
+    const response = await request({
+        uri: site,
+        headers: {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "accept-encoding": "gzip",
+            "accept-language": "en-US,en;q=0.9,pt;q=0.8"
+        },
+        gzip: true
+    });
+
+    let $ = cheerio.load((response));
+    let blogTitle = $(".entry-title").text().trim();
+
+    console.log(blogTitle);
+
+})();
+
+// request('https://beebom.com/android-tv-background-audio-streaming/', (error, response, html) => {
+//     if (!error && response.statusCode == 200) {
+//         const $ = cheerio.load(html);
+
+//         $('.post-preview').each((i, el) => {
+//             const title = $(el)
+//                 .find('.post-title')
+//                 .text()
+//                 .replace(/\s\s+/g, '');
+//             const link = $(el)
+//                 .find('a')
+//                 .attr('href');
+//             const date = $(el)
+//                 .find('.post-date')
+//                 .text()
+//                 .replace(/,/, '');
+//         });

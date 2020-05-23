570e25c8d3dc354bbe2a68034029472df4230eb2
harshithva <harshith11032001@gmail.com>
2020-05-23 13:41:54 +0530

    Now able to scrape images

--- index.js
+++ index.js
@@ -1,45 +1,71 @@
-const request = require('./node_modules/request-promise');
-const cheerio = require('cheerio');
-// const iconv = require('./node_modules/iconv-lite');
-// var encoding = 'utf-8';
+const request = require("./node_modules/request-promise");
+const cheerio = require("cheerio");
+const prompt = require("prompt-sync")();
+const imageDownloader = require("node-image-downloader");
 
-const site = "https://beebom.com/android-tv-background-audio-streaming/";
+// const site = prompt("Enter beebom post link\n");
+// console.log(`Site link: ${site}\n`);
+// console.log(
+//   "-------------------------------------------------------------------\n",
+// );
+const site = "https://beebom.com/spotify-employees-work-from-home-2020/";
 
 (async () => {
-    let data = [];
-    const response = await request({
-        uri: site,
-        headers: {
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "accept-encoding": "gzip",
-            "accept-language": "en-US,en;q=0.9,pt;q=0.8"
-        },
-        gzip: true
-    });
+  let data = [];
+  const response = await request({
+    uri: site,
+    headers: {
+      "accept":
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-encoding": "gzip, deflate,utf-8",
+      "accept-language": "en-US,en;q=0.9,pt;q=0.8",
+    },
+    gzip: true,
+  });
 
-    let $ = cheerio.load((response));
-    let blogTitle = $(".entry-title").text().trim();
-    let description = $('div[class="td-post-content"]').text();
-    let image = $('img').attr('src');
+  let $ = cheerio.load((response), { xmlMode: true });
 
-    console.log(image);
+  let blogTitle = $(".entry-title").text().trim();
 
+  let heroImage = $(
+    ".td-post-content > img",
+  ).attr("src");
+
+  console.log(`Hero Image : ${heroImage}`);
+
+  imageDownloader({
+    imgs: [
+      {
+        uri: heroImage,
+      },
+    ],
+    dest: "./downloads", //destination folder
+  });
+
+  console.log(
+    "-------------------------------------------------------------------\n",
+  );
+
+  let description = $(".td-post-content p").each((i, el) => {
+    const item = $(el).text().trim();
+    console.log(item);
+  });
+
+  console.log(
+    "-------------------------------------------------------------------\n",
+  );
+
+  let images = ["Saab", "Volvo", "BMW"];
+  $("img").each((i, el) => {
+    const item = $(el).attr("src");
+    // console.log(item);
+    images.push(`${item}`);
+  });
+  for (let i = 0; i <= images.length; i++) {
+    console.log(images[i]);
+  }
+  console.log(
+    "-------------------------------------------------------------------\n",
+  );
+  console.log(`Title : ${blogTitle}`);
 })();
-
-// request('https://beebom.com/android-tv-background-audio-streaming/', (error, response, html) => {
-//     if (!error && response.statusCode == 200) {
-//         const $ = cheerio.load(html);
-
-//         $('.post-preview').each((i, el) => {
-//             const title = $(el)
-//                 .find('.post-title')
-//                 .text()
-//                 .replace(/\s\s+/g, '');
-//             const link = $(el)
-//                 .find('a')
-//                 .attr('href');
-//             const date = $(el)
-//                 .find('.post-date')
-//                 .text()
-//                 .replace(/,/, '');
-//         });
